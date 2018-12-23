7da285889f71d980aa06941b9c0f971d1163a4e5
MunifTanjim <muniftanjim@gmail.com>
2018-12-23 14:53:14 +0600

    add major api changes notice

--- src/index.js
+++ src/index.js
@@ -36,4 +36,16 @@ class Bitbucket {
   }
 }
 
+console.warn(
+  `\x1b[43m\x1b[30m %s \x1b[0m\x1b[33m %s \x1b[0m`,
+  `BITBUCKET CLOUD API CHANGING NOTICE:`,
+  `https://developer.atlassian.com/cloud/bitbucket/bitbucket-api-changes-gdpr`
+)
+
+console.info(
+  '\x1b[46m\x1b[30m %s \x1b[0m\x1b[36m %s \x1b[0m',
+  `BITBUCKET CLOUD API MIGRATION GUIDE:`,
+  `https://developer.atlassian.com/cloud/bitbucket/bbc-gdpr-api-migration-guide`
+)
+
 module.exports = Bitbucket
