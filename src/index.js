ae6d9bd169a1344cf055033de7f20c43561ca690
MunifTanjim <muniftanjim@gmail.com>
2019-07-09 07:21:09 +0600

    update entrypoint

--- src/index.js
+++ src/index.js
@@ -1,44 +1,12 @@
-const Hook = require('before-after-hook')
-
-const deepmerge = require('./utils/deepmerge.js')
-const request = require('./request/index.js')
+const Bitbucket = require('./client')
 
 const Plugins = [
-  require('./plugins/authentication/index.js'),
-  require('./plugins/endpoint-methods/index.js'),
-  require('./plugins/pagination/index.js')
+  require('./plugins/notice'),
+  require('./plugins/auth'),
+  require('./plugins/pagination'),
+  require('./plugins/register-endpoints'),
+  require('./plugins/register-api-endpoints'),
+  require('./plugins/validate-request')
 ]
 
-const logNotice = require('./utils/log-notice.js')
-
-const clientDefaults = {
-  headers: {},
-  options: {
-    timeout: 0
-  }
-}
-
-class Bitbucket {
-  constructor(options = {}) {
-    this.options = deepmerge(clientDefaults, options)
-
-    this.hook = new Hook.Collection()
-
-    this.request = this.request.bind(this)
-
-    Plugins.forEach(Plugin => this.addPlugin(Plugin))
-
-    if (!this.options.hideNotice) logNotice()
-    delete this.options.hideNotice
-  }
-
-  addPlugin(Plugin) {
-    new Plugin(this).inject()
-  }
-
-  request(options = {}) {
-    return this.hook('request', deepmerge(this.options, options), request)
-  }
-}
-
-module.exports = Bitbucket
+module.exports = Bitbucket.plugins(Plugins)
