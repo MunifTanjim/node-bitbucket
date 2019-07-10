a1503791a9a3f09054e541f3aff43ff9ab5a6c7d
MunifTanjim <muniftanjim@gmail.com>
2019-07-10 18:26:58 +0600

    add [ts] plugins:pagination

---
+++ src/plugins/pagination/index.ts
@@ -0,0 +1,17 @@
+import { getPage } from './get-page'
+
+type APIClient = import('./types').APIClient
+type PaginatedResponseData<T> = import('./types').PaginatedResponseData<T>
+
+function paginationPlugin(client: APIClient): void {
+  client.hasNextPage = <T>({ next }: PaginatedResponseData<T>): boolean =>
+    Boolean(next)
+  client.getNextPage = getPage.bind(null, client, 'next')
+
+  client.hasPreviousPage = <T>({
+    previous
+  }: PaginatedResponseData<T>): boolean => Boolean(previous)
+  client.getPreviousPage = getPage.bind(null, client, 'previous')
+}
+
+export default paginationPlugin
---
+++ src/plugins/pagination/types.ts
@@ -0,0 +1,6 @@
+export type APIClient = import('../../client/types').APIClient
+export type Direction = 'next' | 'previous'
+export type PaginatedResponseData<
+  T
+> = import('../../request/types').PaginatedResponseData<T>
+export type Response<T> = import('../../request/types').Response<T>
