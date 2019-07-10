833a8feb3fbd7f0d5d3af96d8d4d12e3c5d60e90
MunifTanjim <muniftanjim@gmail.com>
2019-07-10 18:26:49 +0600

    add [ts] plugins:notice

---
+++ src/plugins/notice/types.ts
@@ -0,0 +1,8 @@
+export type APIClient = import('../../client/types').APIClient
+
+export type NoticePluginState = {
+  notice?: boolean
+}
+
+export type Options = import('../../client/types').Options
+export type RequestOptions = import('../../endpoint/types').RequestOptions
