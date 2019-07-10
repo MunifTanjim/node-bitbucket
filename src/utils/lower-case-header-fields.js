b45ffea2b00772b950ffbb4336b8d6f2930b2abd
MunifTanjim <muniftanjim@gmail.com>
2019-07-10 18:25:42 +0600

    add [ts] src/utils

--- src/utils/lower-case-header-fields.js
+++
@@ -1,8 +0,0 @@
-function lowerCaseHeaderFields(headers = {}) {
-  return Object.keys(headers).reduce((newHeaders, key) => {
-    newHeaders[key.toLowerCase()] = headers[key]
-    return newHeaders
-  }, {})
-}
-
-module.exports.lowerCaseHeaderFields = lowerCaseHeaderFields
