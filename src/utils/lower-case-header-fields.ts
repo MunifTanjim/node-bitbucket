12cf061d73356306d90d19f560cddfa2159856b3
MunifTanjim <muniftanjim@gmail.com>
2019-07-13 05:00:10 +0600

    fix src/utils types

--- src/utils/lower-case-header-fields.ts
+++ src/utils/lower-case-header-fields.ts
@@ -1,7 +1,7 @@
 type Headers = import('../endpoint/types').Headers
 
 export function lowerCaseHeaderFields(headers: Headers = {}): Headers {
-  return Object.keys(headers).reduce((newHeaders: Headers, key) => {
+  return Object.keys(headers).reduce((newHeaders: Headers, key): Headers => {
     newHeaders[key.toLowerCase()] = headers[key]
     return newHeaders
   }, {})
