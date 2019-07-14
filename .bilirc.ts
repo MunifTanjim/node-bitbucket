142f5d7693b062f28eb33fa3d2bc1982b06bec12
MunifTanjim <muniftanjim@gmail.com>
2019-07-14 18:17:10 +0600

    remove cjs build

--- .bilirc.ts
+++ .bilirc.ts
@@ -4,7 +4,12 @@ const config: Config = {
   input: ['src/index.ts', 'src/minimal.ts'],
   output: {
     dir: 'lib',
-    format: ['cjs', 'es', 'umd'],
+    fileName: ({ format }) => {
+      return ['es', 'esm'].includes(format)
+        ? `[name][min][ext]`
+        : `[name][min].[format][ext]`
+    },
+    format: ['es', 'umd'],
     minify: true,
     moduleName: 'Bitbucket',
     sourceMapExcludeSources: true
@@ -21,7 +26,7 @@ if (PLUGIN) {
   config.output.dir = 'lib/plugins'
   config.output.format = 'umd'
   config.output.moduleName = `Bitbucket.Plugin.${PLUGIN}`
-  config.output.fileName = `${PLUGIN}.js`
+  config.output.fileName = `${PLUGIN}[min][ext]`
 }
 
 export default config
