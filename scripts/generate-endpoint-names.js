df9b7a167dd4ff944d01da56367410ed29a8d46c
MunifTanjim <muniftanjim@gmail.com>
2020-02-01 13:18:04 +0600

    update scripts/{generate-endpoint-names,generate-routes}

--- scripts/generate-endpoint-names.js
+++ scripts/generate-endpoint-names.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const deepsort = require('deep-sort-object')
+const get = require('lodash/get')
 
 const {
   extractNamespaceFromURL
@@ -16,12 +17,25 @@ const endpointNamesPath = path.resolve(
 
 const ENDPOINT_NAMES = require(endpointNamesPath)
 
+const workspaceRegex = /\/\{workspace\}/
+
 for (const url of Object.keys(PATHS_SPEC)) {
   const resourceNamespaceName = extractNamespaceFromURL(url)
 
   if (!ENDPOINT_NAMES[url]) {
     // new endpoint url
     ENDPOINT_NAMES[url] = {}
+  }
+
+  if (workspaceRegex.test(url)) {
+    const legacyUsernameUrl = url.replace(workspaceRegex, '/{username}')
+
+    const legacyUsernameEndpointNames = get(ENDPOINT_NAMES, legacyUsernameUrl)
+
+    if (legacyUsernameEndpointNames) {
+      ENDPOINT_NAMES[url] = legacyUsernameEndpointNames
+      delete ENDPOINT_NAMES[legacyUsernameUrl]
+    }
   }
 
   for (const method of Object.keys(PATHS_SPEC[url])) {
