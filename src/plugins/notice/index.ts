988adc1e14babdaf0458d235e88388a02b31e8b0
Munif Tanjim <muniftanjim@gmail.com>
2020-07-15 23:06:08 +0600

    update plugins/notice

--- src/plugins/notice/index.ts
+++ src/plugins/notice/index.ts
@@ -9,21 +9,9 @@ function noticePlugin(_client: APIClient, clientOptions: Options): void {
   if (!notice) return
 
   console.log(
-    `\x1b[43m\x1b[30m %s \x1b[0m\x1b[33m %s \x1b[0m`,
-    `BITBUCKET CLOUD API CHANGING NOTICE:`,
-    `https://developer.atlassian.com/cloud/bitbucket/bitbucket-api-changes-gdpr`
-  )
-
-  console.log(
     '\x1b[46m\x1b[30m %s \x1b[0m\x1b[36m %s \x1b[0m',
-    `BITBUCKET CLOUD API MIGRATION GUIDE:`,
-    `https://developer.atlassian.com/cloud/bitbucket/bbc-gdpr-api-migration-guide`
-  )
-
-  console.log(
-    '\x1b[45m\x1b[30m %s \x1b[0m\x1b[35m %s \x1b[0m',
-    `BITBUCKET CLOUD API REPO URL CHANGE:`,
-    `https://developer.atlassian.com/cloud/bitbucket/bitbucket-api-changes-workspaces`
+    `BITBUCKET CLOUD API LATEST UPDATES:`,
+    `https://developer.atlassian.com/cloud/bitbucket`
   )
 }
 
